8289aa8dd3d29d6b5e51f359b1f3ad93af013327
Luna <ludmilamodna3@gmail.com>
2025-03-11 14:34:13 +0200

    Added homework

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+//1
+
+for (let i = 25; i >= 0; i--) {
+  console.log(i);
+}
+
+//2
+
+let num = 10;
+do {
+  console.log(num);
+  num += 5;
+} while (num <= 50);
+
+//3
+
+let number = 1;
+let sum = 0;
+
+while (number <= 100) {
+  sum += number;
+  number++;
+}
+console.log(sum);
