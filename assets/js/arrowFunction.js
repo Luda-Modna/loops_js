8a9e0e931c91b3af1828624d5d606a1919c067da
Luna <ludmilamodna3@gmail.com>
2025-03-18 09:38:21 +0200

    Added arrayPractic.js

--- assets/js/arrowFunction.js
+++ assets/js/arrowFunction.js
@@ -1,95 +1,23 @@
+//стрілочна функція
+const arrow = (a, b) => {
+  return a + b;
+};
 
-//сума
+console.log(arrow(4, 5));
 
-function sum(...sumands) {
-  function howToReduce(accum, item) {
-    return (accum += item);
-  }
-  const sum = sumands.reduce(howToReduce);
-  return sum;
-}
+//isAdult
+const isAdult = (age) => age >= 18;
 
-const res1 = sum(1, 2, 3, 4, 5, 6);
-console.log(res1);
+console.log(isAdult(18));
 
-//якщо додаємо то обраховується сума
-//якщо множимо то добуток
+//greeting
 
-function calc(a, ...commands) {
-  function calcComm(accum, item) {
-    if (a === "+") {
-      return (accum += item);
-    } else if (a === "*") {
-      return (accum *= item);
-    } else {
-      return null;
-    }
-  }
-  const result = commands.reduce(calcComm);
-  return result;
-}
+const greetingObj = {
+  en: "Hello",
+  ua: "Вітаю",
+};
 
-const res2 = calc("*", 1, 2, 3, 4, 5, 6);
-console.log(res2);
+const greeting = (lang, userName) => `${greetingObj[lang]}, ${userName}!`;
 
-//привіт + ім'я користувача
-
-function greet(greetings, ...names) {
-  let hey = "";
-  for (let i = 0; i < names.length; i++) {
-    hey += greetings + ", " + names[i] + "! ";
-  }
-  return hey;
-}
-
-const greetingsUsers = greet("Hey", "Anna", "Max", "Vita");
-console.log(greetingsUsers);
-
-//знайшла найбільші числа
-
-function findMax(...num) {
-  return Math.max(...num);
-}
-
-console.log(findMax(1, 2, 9, 5));
-
-
-//середнє арифметичне
-
-function average(...numbers) {
-  function sumNumber(accum, item) {
-    return (accum += item);
-  }
-  const sum = numbers.reduce(sumNumber);
-  return sum / numbers.length;
-}
-
-console.log(average(10, 20, 30));
-console.log(average(5, 15));
-
-//виведення категорій і продуктів
-
-function listItems(category, ...products) {
-  let res1 = `Category: ${category}.`;
-  let res2 = ` Products: ${products}.`;
-
-  const result = res1 + res2;
-  return result;
-}
-
-console.log(listItems("Fructs", "Apple", " Banana", " Orange"));
-console.log(listItems("Drinks", "Tea", " Coffee"));
-
-//ввиведення парних чисел
-
-function filterEven(...num1) {
-  let evenNumb = [];
-  for (let i = 1; i < num1.length; i++) {
-    if (num1[i] % 2 === 0) {
-      evenNumb.push(num1[i]);
-    }
-  }
-  return evenNumb;
-}
-
-console.log(filterEven(1, 2, 3, 4, 5, 6));
+console.log(greeting("ua", "Анна"));
+console.log(greeting("en", "Max"));
